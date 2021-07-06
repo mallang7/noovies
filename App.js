26379297fedc7f427a3c32af120c6af107b9232a
mallang7 <seonug0725@gmail.com>
2021-07-06 23:43:06 +0900

    .

--- App.js
+++ App.js
@@ -1,17 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { AppLoading } from 'expo';
-import { StyleSheet, Text, View } from 'react-native';
+import { AppLoading } from 'expo-app-loading';
+import { ImageStore, StyleSheet, Text, View } from 'react-native';
 
+const cacheImages = (images) => images.map(image => {
+  if (typeof image === "string"){
+    return Image.prefetch(image)
+  } else {
+    return Asset.fromModule(image).downloadAsync();
+  }
+})
 
 export default function App() {
   const [isReady, setIsReady] = useState(false)
   const loadAssets = async () =>{};
-  const onfinish = () => setIsReady(true);
-  return isReady ? null : (<AppLoading 
+  const onFinish = () => setIsReady(true);
+  return isReady ? null : (
+  <AppLoading 
   startAsync={loadAssets} 
   onFinish={onFinish} 
   onError={console.error} />);
 }
-
-
