8721597bcbd9c5ca9a8118531ff5e642e72c8d18
mallang7 <seonug0725@gmail.com>
2021-08-03 14:25:01 +0900

    .

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { AppLoading } from 'expo-app-loading';
-import { ImageStore, StyleSheet, Text, View } from 'react-native';
+import { ImageStore, StyleSheet, Text, View, Image } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
 const cacheImages = (images) => images.map(image => {
   if (typeof image === "string"){
@@ -12,8 +13,11 @@ const cacheImages = (images) => images.map(image => {
 })
 
 export default function App() {
-  const [isReady, setIsReady] = useState(false)
-  const loadAssets = async () =>{};
+  const [isReady, setIsReady] = useState(false);
+  const loadAssets = async () => {
+    const images = cacheImages(["https://unsplash.com/photos/D00DEMhNptA",
+  require("./assets/splash.png")])
+  };
   const onFinish = () => setIsReady(true);
   return isReady ? null : (
   <AppLoading 
