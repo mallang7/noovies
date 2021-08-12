f6f5f83365d320f34cf0df446eef6f8e82a57ae1
mallang7 <seonug0725@gmail.com>
2021-08-12 14:36:24 +0900

    .

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -4,13 +4,24 @@ import Movies from "../screens/Movies";
 import Tv from "../screens/Tv";
 import Search from "../screens/Search";
 import Favs from "../screens/Favs";
+import { useEffect } from "react";
 
 const Tabs = createBottomTabNavigator();
 
-export default () => (<Tabs.Navigator>
-    <Tabs.Screen name="Movies" component={Movies}/>
-    <Tabs.Screen name="Tv" component={Tv}/>
-    <Tabs.Screen name="Search" component={Search}/>
-    <Tabs.Screen name="Favs" component={Favs}/>
-    </Tabs.Navigator>
-    );
+const getHeaderName = (route) => route?.state?.routeNames[route.state.index] || "Movies"
+
+export default ({navigation, route}) => {
+    useLayoutEffect( () => {
+        navigation.setOptions({
+            title: getHeaderName(route)
+        });
+    }, [route]);
+    return(
+        <Tabs.Navigator>
+            <Tabs.Screen name="Movies" component={Movies}/>
+            <Tabs.Screen name="Tv" component={Tv}/>
+            <Tabs.Screen name="Search" component={Search}/>
+            <Tabs.Screen name="Favourites" component={Favs}/>
+            </Tabs.Navigator>
+            );
+}
